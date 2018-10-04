d218a70eb8ac6c019d835e1ced3d8da6068cc47d
icebergnevercries <denislosik@mail.ru>
2018-10-05 01:37:08 +0300

    Implementation class, which works as calculator

--- src/index.js
+++ src/index.js
@@ -1,26 +1,95 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.solution = [];
+    this.solution.push(initialValue);
+    this.modifsolution = [];
+    this.lenghtarr = 1;
+    this.modiflenghtarr = 1;
+    this.flag = true;
   }
 
   add(number) {
-    // your implementation
+    this.solution.push('+');
+    this.solution.push(number);
+    this.lenghtarr += 2;
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.solution.push('-');
+    this.solution.push(number);
+    this.lenghtarr += 2;
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.solution.push('*');
+    this.solution.push(number);
+    this.lenghtarr += 2;
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.solution.push('/');
+    this.solution.push(number);
+    this.lenghtarr += 2;
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.solution.push('**');
+    this.solution.push(number);
+    this.lenghtarr += 2;
+    return this;
+  }
+
+  valueOf() {
+
+    if(this.flag){
+      
+      for(let i = 0; i < this.lenghtarr/* && this.flag*/; i++){
+        this.modifsolution.push(this.solution[i]);
+        if(i == this.lenghtarr - 1){        
+          this.flag = false;
+          this.modiflenghtarr = this.lenghtarr;
+        }
+      }    
+  
+      for(let i = this.modiflenghtarr; i >= 0 ; i -= 2 ){
+        if(this.modifsolution[i] == '**'){
+          this.modifsolution.splice(i - 1, 3, this.modifsolution[i - 1] ** this.modifsolution[i + 1]);    
+          this.modiflenghtarr -= 2;    
+        }
+      }
+  
+      for(let i = 1; i < this.modiflenghtarr; i += 2){
+        if(this.modifsolution[i] == '*'){
+          this.modifsolution.splice(i - 1, 3, this.modifsolution[i - 1] * this.modifsolution[i + 1]);    
+          this.modiflenghtarr -= 2;    
+          i -= 2;
+        }
+        else if(this.modifsolution[i] == '/'){
+          this.modifsolution.splice(i - 1, 3, this.modifsolution[i - 1] / this.modifsolution[i + 1]);    
+          this.modiflenghtarr -= 2;    
+          i -= 2;
+        }
+      }
+  
+      for(let i = 1; i < this.modiflenghtarr; i += 2){
+        if(this.modifsolution[i] == '+'){
+          this.modifsolution.splice(i - 1, 3, this.modifsolution[i - 1] + this.modifsolution[i + 1]);    
+          this.modiflenghtarr -= 2;    
+          i -= 2;
+        }
+        else if(this.modifsolution[i] == '-'){
+          this.modifsolution.splice(i - 1, 3, this.modifsolution[i - 1] - this.modifsolution[i + 1]);    
+          this.modiflenghtarr -= 2;    
+          i -= 2;
+        }
+      } 
+    }
+
+    return this.modifsolution[0];
   }
 }
 
